2d1f28aa1799748230c144805932e1ac8bf631f8
Michael Manges <michael.manges@gmail.com>
2018-04-12 15:08:36 +0400

    Improve keyboard logic

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import keyboard from '../logic/keyboard';
+import keyboardTest from '../logic/keyboardTest';
 import EventLister, { withOptions } from 'react-event-listener';
 import '../styles/button.css';
 
@@ -8,23 +8,39 @@ class Button extends React.Component {
     clicked: false,
   };
 
-  handleClick = (e) => {
-    let { key } = e;
-    if (keyboard(key)) {
-      if (key === 'p') { key = '+'; }
-      if (key === 'm') { key = '−'; }
-      if (key === 'x') { key = '×'; }
-      if (key === 'd') { key = '÷'; }
-      if (key === 'n') { key = '±'; }
-      if (key === 's') { key = '√'; }
-      if (key === ' ') { key = 'c'; }
-      if (key === 'Enter') { key = '='; }
-    } else { return; }
-    
-    const buttonName = key || this.props.name;
+  handleClick = () => {
+    // const key = this.keyboard(e);
+    // const buttonName = key ? key : this.props.name;
+    const buttonName = this.props.name;
     this.props.clickHandler(buttonName);
-    this.setState({ clicked: true });
   };
+
+  keyboard = (keyName) => {
+    const { key } = keyName;
+    let newKey;
+    if (keyboardTest(key)) {
+      if (key === 'p') { newKey = '+'; }
+      if (key === 'm') { newKey = '−'; }
+      if (key === 'x') { newKey = '×'; }
+      if (key === 'd') { newKey = '÷'; }
+      if (key === 'n') { newKey = '±'; }
+      if (key === 's') { newKey = '√'; }
+      if (key === ' ') { newKey = 'c'; }
+      if (key === '.') { newKey = '.'; }
+      if (key === 'Enter') { newKey = '='; }
+      if (key === '0') { newKey = '0'; }
+      if (key === '1') { newKey = '1'; }
+      if (key === '2') { newKey = '2'; }
+      if (key === '3') { newKey = '3'; }
+      if (key === '4') { newKey = '4'; }
+      if (key === '5') { newKey = '5'; }
+      if (key === '6') { newKey = '6'; }
+      if (key === '7') { newKey = '7'; }
+      if (key === '8') { newKey = '8'; }
+      if (key === '9') { newKey = '9'; }
+    } else { return; }
+    this.props.clickHandler(newKey);
+  }
 
   isClicked = () => {
     console.log('is clicked');
@@ -44,7 +60,7 @@ class Button extends React.Component {
         {this.props.name}
         <EventLister
           target="document"
-          onKeyDown={withOptions(this.handleClick, { passive: true, capture: false, } )}
+          onKeyDown={withOptions(this.keyboard, { passive: true, capture: false, } )}
         />
       </div>
     ); 
--- src/logic/keyboard.js
+++
@@ -1,6 +0,0 @@
-// ignore keyboard keys pressed that do not
-// have any calculator function
-
-const keyboard = keyName => /(^p$|^m$|^x$|^d$|^n$|^s$|^ $|^Enter$|[0-9]+)/.test(keyName);
-
-export default keyboard;
