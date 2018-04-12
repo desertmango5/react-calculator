6f950a2f49bf006214e0b34b070bcaf15310c8f0
Michael Manges <michael.manges@gmail.com>
2018-04-12 15:26:29 +0400

    Refactor keyboard logic

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import keyboardTest from '../logic/keyboardTest';
 import EventLister, { withOptions } from 'react-event-listener';
 import '../styles/button.css';
 
@@ -9,36 +8,33 @@ class Button extends React.Component {
   };
 
   handleClick = () => {
-    // const key = this.keyboard(e);
-    // const buttonName = key ? key : this.props.name;
     const buttonName = this.props.name;
     this.props.clickHandler(buttonName);
   };
 
-  keyboard = (keyName) => {
-    const { key } = keyName;
+  keyboardClick = (keyName) => {
+    const { which } = keyName;
     let newKey;
-    if (keyboardTest(key)) {
-      if (key === 'p') { newKey = '+'; }
-      if (key === 'm') { newKey = '−'; }
-      if (key === 'x') { newKey = '×'; }
-      if (key === 'd') { newKey = '÷'; }
-      if (key === 'n') { newKey = '±'; }
-      if (key === 's') { newKey = '√'; }
-      if (key === ' ') { newKey = 'c'; }
-      if (key === '.') { newKey = '.'; }
-      if (key === 'Enter') { newKey = '='; }
-      if (key === '0') { newKey = '0'; }
-      if (key === '1') { newKey = '1'; }
-      if (key === '2') { newKey = '2'; }
-      if (key === '3') { newKey = '3'; }
-      if (key === '4') { newKey = '4'; }
-      if (key === '5') { newKey = '5'; }
-      if (key === '6') { newKey = '6'; }
-      if (key === '7') { newKey = '7'; }
-      if (key === '8') { newKey = '8'; }
-      if (key === '9') { newKey = '9'; }
-    } else { return; }
+      if (which === 80) { newKey = '+'; }
+      if (which === 77) { newKey = '−'; }
+      if (which === 88) { newKey = '×'; }
+      if (which === 68) { newKey = '÷'; }
+      if (which === 78) { newKey = '±'; }
+      if (which === 83) { newKey = '√'; }
+      if (which === 32) { newKey = 'c'; }
+      if (which === 190) { newKey = '.'; }
+      if (which === 13) { newKey = '='; }
+      if (which === 48) { newKey = '0'; }
+      if (which === 49) { newKey = '1'; }
+      if (which === 50) { newKey = '2'; }
+      if (which === 51) { newKey = '3'; }
+      if (which === 52) { newKey = '4'; }
+      if (which === 53) { newKey = '5'; }
+      if (which === 54) { newKey = '6'; }
+      if (which === 55) { newKey = '7'; }
+      if (which === 56) { newKey = '8'; }
+      if (which === 57) { newKey = '9'; }
+      if (!newKey) { return; }
     this.props.clickHandler(newKey);
   }
 
@@ -60,7 +56,7 @@ class Button extends React.Component {
         {this.props.name}
         <EventLister
           target="document"
-          onKeyDown={withOptions(this.keyboard, { passive: true, capture: false, } )}
+          onKeyDown={withOptions(this.keyboardClick, { passive: true, capture: false, } )}
         />
       </div>
     ); 
--- src/logic/keyboardTest.js
+++
@@ -1,6 +0,0 @@
-// ignore keyboard keys pressed that do not
-// have any calculator function
-
-const keyboardTest = keyName => /(^p$|^m$|^x$|^d$|^n$|^s$|^ $|^Enter$|^0$|^1$|^2$|^3$|^4$|^5$|^6$|^7$|^8$|^9$|^.$)/.test(keyName);
-
-export default keyboardTest;
