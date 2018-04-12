4fac174807378acbf20a91521f8a5695a40430f0
Michael Manges <michael.manges@gmail.com>
2018-04-12 12:55:56 +0400

    Keyboard filter to ignore not functional keys

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,38 +1,55 @@
 import React from 'react';
+import keyboard from '../logic/keyboard';
 import EventLister, { withOptions } from 'react-event-listener';
 import '../styles/button.css';
 
-const Button = (props) => {
-  const handleClick = (e) => {
-    let { key } = e;
-    if (key === 'p') { key = '+'; }
-    if (key === 'm') { key = '−'; }
-    if (key === 'x') { key = '×'; }
-    if (key === 'd') { key = '÷'; }
-    if (key === 'n') { key = '±'; }
-    if (key === 's') { key = '√'; }
-    if (key === ' ') { key = 'c'; }
-    if (key === 'Enter') { key = '='; }
-    
-    const buttonName = key || props.name;
-    props.clickHandler(buttonName);
+class Button extends React.Component {
+  state = {
+    clicked: false,
   };
 
-  const buttonClicked = () => 'button__clicked';
-  const buttonColors = () => (props.red ? 'button red' : 
-    props.black ? 'button black' : 'button');
+  handleClick = (e) => {
+    let { key } = e;
+    if (keyboard(key)) {
+      if (key === 'p') { key = '+'; }
+      if (key === 'm') { key = '−'; }
+      if (key === 'x') { key = '×'; }
+      if (key === 'd') { key = '÷'; }
+      if (key === 'n') { key = '±'; }
+      if (key === 's') { key = '√'; }
+      if (key === ' ') { key = 'c'; }
+      if (key === 'Enter') { key = '='; }
+    } else { return; }
+    
+    const buttonName = key || this.props.name;
+    this.props.clickHandler(buttonName);
+    this.setState({ clicked: true });
+  };
 
-  const className = [buttonColors()];
+  isClicked = () => {
+    console.log('is clicked');
+  };
+
+  render() {
+    const { clicked } = this.state;
+
+    const buttonClicked = () => clicked ? 'button__clicked' : '';
+    const buttonColors = () => (this.props.red ? 'button red' : 
+      this.props.black ? 'button black' : 'button');
   
-  return (
-    <div className={className.join(' ')} onClick={handleClick}>
-      {props.name}
-      <EventLister 
-        target="document"
-        onKeyDown={handleClick}
-      />
-    </div>
-  ); 
-};
+    const className = [buttonColors(), buttonClicked()];
+    
+    return (
+      <div className={className.join(' ')} onClick={this.handleClick}>
+        {this.props.name}
+        <EventLister
+          target="document"
+          onKeyDown={withOptions(this.handleClick, { passive: true, capture: false, } )}
+        />
+      </div>
+    ); 
+  };
+}
+
 
 export default Button;
